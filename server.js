81adb573d943525fcf82141c73bcbb50f5b2b469
Паша Горбатюк <92729703+PavloHorbatiuk@users.noreply.github.com>
2022-09-05 09:51:31 +0300

    fixed .gitignore

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
+
+
 const formatMesage = (userName, text) => {
 	return {
 		userName,
