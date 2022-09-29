0b87d7385a6ee0e0e71a11d7119f71867d93d984
Roman Romanenko <romanenko1996@meta.ua>
2022-09-29 14:44:50 +0300

    fixed inline style conditional statement

--- client/src/pages/GamePage.jsx
+++ client/src/pages/GamePage.jsx
@@ -10,12 +10,12 @@ import { DisplayTimer } from './../components/GameDetails/DisplayTimer';
 
 let socket;
 function GamePage() {
-	const navigate = useNavigate();
-	const { roomId, userName, roomLimit, gameDuration } = useParams();
-	const [roomUsers, setRoomUsers] = useState([]);
-	const [data, setData] = useState([]);
-	const [gameStarted, setGameStarted] = useState(false);
-	const [timeLeft, setTimeLeft] = useState(5);
+  const navigate = useNavigate();
+  const { roomId, userName, roomLimit, gameDuration } = useParams();
+  const [roomUsers, setRoomUsers] = useState([]);
+  const [data, setData] = useState([]);
+  const [gameStarted, setGameStarted] = useState(false);
+  const [timeLeft, setTimeLeft] = useState(5);
   const [gameTime, setGameTime] = useState(gameDuration);
 
   const isTimeUp = timeLeft === 0;
@@ -30,82 +30,79 @@ function GamePage() {
   }, [roomUsers.length, roomLimit]);
 
   useEffect(() => {
-    if (!gameStarted && isTimeUp) {
-      setGameStarted(true);
-    }
+    if (!gameStarted && isTimeUp) setGameStarted(true);
+
   }, [isTimeUp]);
 
   useEffect(() => {
-      if (gameStarted) {
-        const intervalId = setInterval(() => {
-          setGameTime(prev => prev - 1);
-        }, 1000);
-  
-        gameTimeout.current = setTimeout(() => {
-          setGameStarted(false);
-           clearInterval(intervalId);
-        }, 1000 * gameDuration);
-      }
-    }, [gameStarted])
+    if (gameStarted) {
+      const intervalId = setInterval(() => setGameTime(prev => prev - 1), 1000);
 
-	useEffect(() => {
-		socket = io(SOCKET_URL, { transports: ['websocket'] });
-		const handler = msg => setData(prev => [...prev, msg]);
-		socket.on('message', handler);
-		socket.emit('joinRoom', { roomId, userName, roomLimit, gameDuration });
-		socket.on('roomUsers', ({ room, users }) => {
-			setRoomUsers(users);
-		});
-		return () => {
-			socket.disconnect();
-		};
-	}, [roomId, roomLimit, userName, gameDuration]);
+      gameTimeout.current = setTimeout(() => {
+        setGameStarted(false);
+        clearInterval(intervalId);
+      }, 1000 * gameDuration);
+    }
+  }, [gameStarted])
 
-	const countHandler = (count, setCount) => {
-		socket.emit('userMsg', count);
-		setCount(prev => prev + 1);
-	};
-	const winner = data.sort((a, b) => b.text - a.text)[0]?.userName;
-	const isStartedStyle = gameStarted
-		? styles.startGame
-		: styles.startGameHidden;
+  useEffect(() => {
+    socket = io(SOCKET_URL, { transports: ['websocket'] });
+    const handler = msg => setData(prev => [...prev, msg]);
+    socket.on('message', handler);
+    socket.emit('joinRoom', { roomId, userName, roomLimit, gameDuration });
+    socket.on('roomUsers', ({ room, users }) => {
+      setRoomUsers(users);
+    });
+    return () => {
+      socket.disconnect();
+    };
+  }, [roomId, roomLimit, userName, gameDuration]);
 
-	return (
-		<div className={styles.gameWrapper}>
-			<div style={{ display: 'flex' }}>
-				<button
-					title="COPY"
-					className={styles.buttonEnterName}
-					onClick={() => copy()}
-				>
-					{COPY_LABEL}
-				</button>
-				<button
-					title="GO HOME"
-					className={styles.buttonEnterName}
-					onClick={() => navigate('/')}
-				>
-					{GO_HOME_LABEL}
-				</button>
-			</div>
-			<div className={isStartedStyle}>GO</div>
-			<div>
-				<ClickCount
-					userName={userName}
-					gameStarted={gameStarted}
-					countHandler={countHandler}
-				/>
-				<DisplayTimer
-					roomUsers={roomUsers}
-					winner={winner}
-					gameStarted={gameStarted}
-					timeLeft={timeLeft}
-					data={data}
+  const countHandler = (count, setCount) => {
+    socket.emit('userMsg', count);
+    setCount(prev => prev + 1);
+  };
+  const winner = data.sort((a, b) => b.text - a.text)[0]?.userName;
+  const isStartedStyle = gameStarted
+    ? styles.startGame
+    : styles.startGameHidden;
+
+  return (
+    <div className={styles.gameWrapper}>
+      <div style={{ display: 'flex' }}>
+        <button
+          title="COPY"
+          className={styles.buttonEnterName}
+          onClick={() => copy()}
+        >
+          {COPY_LABEL}
+        </button>
+        <button
+          title="GO HOME"
+          className={styles.buttonEnterName}
+          onClick={() => navigate('/')}
+        >
+          {GO_HOME_LABEL}
+        </button>
+      </div>
+      <div className={isStartedStyle}>GO</div>
+      <div>
+        <ClickCount
+          userName={userName}
+          gameStarted={gameStarted}
+          countHandler={countHandler}
+        />
+        <DisplayTimer
+          roomUsers={roomUsers}
+          winner={winner}
+          gameStarted={gameStarted}
+          timeLeft={timeLeft}
+          data={data}
           gameTime={gameTime}
-				/>
-			</div>
-		</div>
-	);
+        />
+      </div>
+    </div>
+  );
 }
 
 export default GamePage;
